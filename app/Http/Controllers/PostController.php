99e6a40bd1e1e2459854ada2bfed02e6ff81e7c3
SHINNK-KS <izumiyou963@gmail.com>
2024-12-14 20:16:52 +0900

    controller修正しました

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ControllerKadai extends Controller
+class PostController extends Controller
 {
     public function index() {
-    return view('index');
+        return view('index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ControllerKadai;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/post', [ControllerKadai::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
