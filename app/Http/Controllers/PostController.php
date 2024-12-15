55fa3f4f0aac47f054173d5da2f0dd2b152f713e
SHINNK-KS <izumiyou963@gmail.com>
2024-12-15 13:26:25 +0900

    課題upload

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use function Laravel\Prompts\table;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('index');
+        $posts = DB::table('posts')->get();
+
+        return view('posts.index', compact('posts'));
+        
     }
 }
