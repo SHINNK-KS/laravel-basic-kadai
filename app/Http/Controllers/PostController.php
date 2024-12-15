610029678b55ea8a94af51565ed48c1b4b99db0d
SHINNK-KS <izumiyou963@gmail.com>
2024-12-15 23:36:02 +0900

    use Eloquent ORM to get data

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Post;
 
 use function Laravel\Prompts\table;
 
@@ -13,6 +14,10 @@ class PostController extends Controller
         $posts = DB::table('posts')->get();
 
         return view('posts.index', compact('posts'));
-        
+    }
+
+    public function show($id) {
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
     }
 }
