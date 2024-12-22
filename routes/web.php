29d423a47d062b2c42665887780459af87b8fd85
SHINNK-KS <izumiyou963@gmail.com>
2024-12-22 13:58:33 +0900

    Implementing Authentication & Authorization

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ControllerKadai;
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
+
 
 
 /*
@@ -19,11 +21,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create')->middleware('auth');
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
